692cce50eb2a25b9c073124597b7828fea7c31e7
Abdullah Al-Noman <nomanfcb@gmail.com>
2024-01-27 03:57:09 +0600

    Route has been updated in web.php

--- Laravel_Api/routes/web.php
+++ Laravel_Api/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
+});
+
